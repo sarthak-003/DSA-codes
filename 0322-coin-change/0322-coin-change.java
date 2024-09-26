b132e833518a81f7b1c7eda33049f14e7ad0c984
sarthak-003 <139106184+sarthak-003@users.noreply.github.com>
2024-09-27 02:43:34 +0530

    Time: 19 ms (41.84%), Space: 45.2 MB (18.77%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,30 +1,35 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);
-        //return coinChangeRec(coins,coins.length-1,amount);
-
+    
         int[][] dp = new int[coins.length][amount + 1];
-        for(int[] ele : dp) Arrays.fill(ele,-1);
-        int ans = Memoization(coins,coins.length-1,amount,dp);
-        //for(int[] ele : dp) System.out.println(Arrays.toString(ele));
-        if(ans == 10001) return -1;
-        return ans;
+        for(int[] ele : dp) Arrays.fill(ele,10001);
+        
+        return tabulation(coins,amount,dp);
     }
 
-    static int Memoization(int[] coins,int i,int amount,int[][] dp){
-        if(i == 0) {
-            if (amount % coins[i] == 0) {
-                return amount / coins[i];
+    
+    static int tabulation(int[] coins,int amount,int[][] dp){
+        for(int i = 0 ; i < dp.length; i++){
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= amount; j++) {
+            if(j % coins[0] == 0){
+                dp[0][j] = j / coins[0];
             }
-            return 10001;
         }
-        if (amount % coins[i] == 0) {
-            return dp[i][amount] = amount / coins[i];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j <= amount; j++) {
+                int notPick = dp[i-1][j];
+                int pick = 10001;
+                if(j >= coins[i]){
+                    pick = 1+dp[i][j-coins[i]];
+                }
+                dp[i][j] = Integer.min(pick,notPick);
+            }
         }
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int pick = 10001;
-        if(amount >= coins[i]) pick = 1 + Memoization(coins,i,amount - coins[i],dp);
-        int notPick = Memoization(coins,i-1,amount,dp);
-        return dp[i][amount] =Integer.min(pick,notPick) ;
+
+        if(dp[dp.length-1][amount] >= 10001) return -1;
+        return dp[dp.length-1][amount];
     }
 }
