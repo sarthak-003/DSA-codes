9a507a415783461da0d870a496973d93d848a970
sarthak-003 <139106184+sarthak-003@users.noreply.github.com>
2024-10-01 15:37:36 +0530

    Time: 7 ms (82.22%), Space: 44.7 MB (57.1%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minDistance(String str1, String str2) {
+        int[][] dp = new int[str1.length()][str2.length()];
+        for(int[] ele : dp) Arrays.fill(ele, -1);
+        int lcs =  Memoization(str1.length()-1,str2.length()-1,str1,str2,dp);
+        return  str1.length() + str2.length() - 2 * lcs;
+    }
+    static int Memoization(int s1,int s2,String str1,String str2,int[][] dp){
+        if(s1<0 || s2<0) return 0;
+        if(s1==0 && s2==0){
+            if(str1.charAt(s1) == str2.charAt(s2)) return 1;
+            return  0;
+        }
+
+        if(dp[s1][s2] != -1) return dp[s1][s2];
+        char a = str1.charAt(s1);
+        char b = str2.charAt(s2);
+        int match=0,notMatch = 0;
+        if(a==b){
+            match = 1 + Memoization(s1-1,s2-1,str1,str2,dp);
+        }
+        else{
+            notMatch = Integer.max(Memoization(s1-1,s2,str1,str2,dp),Memoization(s1,s2-1,str1,str2,dp));
+        }
+        return dp[s1][s2] = match + notMatch;
+    }
+}
